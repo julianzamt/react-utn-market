362f2bd8b546d94800e5768fe6bff32bf0c7bc93
Julian Zamt <julizamt@gmail.com>
2021-05-03 16:12:24 -0300

    Added metatags for social media

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,28 +6,28 @@ import Alert from 'react-bootstrap/Alert'
 import AppContext from "../context/AppContext"
 import LinearProgress from '@material-ui/core/LinearProgress'
 
-function Home(){
-    const [ products, setProducts ] = useState("")
-    const [ isLoading, setIsLoading ] = useState(false)
+function Home() {
+    const [products, setProducts] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
 
     const context = useContext(AppContext)
 
     useEffect(() => {
         setIsLoading(true)
         firebase.db.collection("Productos")
-        .get()
-        .then((querySnapshot) => {
-            const products = querySnapshot.docs.map(item =>  
-            <Producto 
-            key={item.id}
-            id={item.id} 
-            name={item.data().name} 
-            price={item.data().price}
-            photo_url={item.data().photo_url}
-            />)
-            setProducts(products)
-            setIsLoading(false)
-        })
+            .get()
+            .then((querySnapshot) => {
+                const products = querySnapshot.docs.map(item =>
+                    <Producto
+                        key={item.id}
+                        id={item.id}
+                        name={item.data().name}
+                        price={item.data().price}
+                        photo_url={item.data().photo_url}
+                    />)
+                setProducts(products)
+                setIsLoading(false)
+            })
     }, [])
 
     const closeRegistryAlert = () => {
@@ -39,14 +39,14 @@ function Home(){
     }
     else {
         return (
-        <div className="text-center">
-            {context.registryFeedback ? 
-            <Alert variant="success" onClose={closeRegistryAlert} dismissible>User successfully registered and logged in.</Alert> 
-            : null}
-            <CardDeck style={{margin:"2em"}}>
-                {products}
-            </CardDeck>
-        </div>
+            <div className="text-center">
+                {context.registryFeedback ?
+                    <Alert variant="success" onClose={closeRegistryAlert} dismissible>User successfully registered and logged in.</Alert>
+                    : null}
+                <CardDeck style={{ margin: "2em" }}>
+                    {products}
+                </CardDeck>
+            </div>
         )
     }
 }
